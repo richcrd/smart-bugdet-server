29f554ec69d4aabd84fabd1686a8f54153b32cb0
Richard Rodriguez <fam.castro99@gmail.com>
2026-07-01 17:07:16 -0600

    Add notification method to list and mark as read

--- SMB.APPLICATION/Interfaces/Services/INotificationService.cs
+++ SMB.APPLICATION/Interfaces/Services/INotificationService.cs
@@ -8,4 +8,7 @@ public interface INotificationService
     Task<bool> SendDailyReminderIfNeeded(long userId);
     Task SendDailyReminders();
     Task CheckBalanceAlert(long userId, decimal currentBalance, string currencySymbol);
+    Task<List<NotificationResponse>> GetNotifications(long userId);
+    Task MarkAsRead(long userId, long notificationId);
+    Task MarkAllAsRead(long userId);
 }
---
+++ SMB.INFRASTRUCTURE/Persistence/Configurations/NotificationConfiguration.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SMB.DOMAIN.Entities;
+
+namespace SMB.INFRASTRUCTURE.Persistence.Configurations;
+
+public class NotificationConfiguration : IEntityTypeConfiguration<Notification>
+{
+    public void Configure(EntityTypeBuilder<Notification> builder)
+    {
+        builder.ToTable("notifications");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Title)
+            .HasMaxLength(200)
+            .IsRequired();
+
+        builder.Property(x => x.Body)
+            .HasMaxLength(500)
+            .IsRequired();
+
+        builder.HasIndex(x => new { x.UserId, x.IsRead });
+
+        builder.HasOne(x => x.User)
+            .WithMany(x => x.Notifications)
+            .HasForeignKey(x => x.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
